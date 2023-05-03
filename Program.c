ade491d95aba75371f82d11624a1015f396e1191
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-03 21:45:01 +0530

    Update and rename F° to C° to Program.c

--- Program.c
+++ Program.c
@@ -1,4 +1,7 @@
-/* Convert iqnto Fahrenheit to Centigrate */
+- Temperature of a city in Fahrenheit degrees is input through the keyboard. 
+- Write a program to convert this temperature into Centigrade degrees.
+
+
 #include <stdio.h>
 #include <conio.h>
 void main()
